ab9809ec01f1811ddd3245f848ce361f3caaba1a
Caleb Fletcher <caleb@fletcher.cf>
2022-11-27 15:40:01 +1100

    feat: add database version number
    Fixes #5

--- src/db.rs
+++ src/db.rs
@@ -1,23 +1,53 @@
 use std::path::Path;
 
-use anyhow::Context;
+use anyhow::{anyhow, Context};
+use tracing::warn;
 
 use crate::Entry;
 
+const DB_VERSION: u32 = 2;
+static DB_VERSION_KEY: &str = "version";
+
 #[derive(Debug, Clone)]
 pub struct Db {
-    inner: sled::Db,
+    crate_tree: sled::Tree,
 }
 
 impl Db {
     pub fn open(path: impl AsRef<Path>) -> Result<Self, anyhow::Error> {
         let db = sled::open(path).with_context(|| "unable to open database")?;
 
-        Ok(Db { inner: db })
+        match db.get(DB_VERSION_KEY)? {
+            Some(version_bytes) => {
+                let version: u32 = bincode::deserialize(&version_bytes)
+                    .with_context(|| "could not deserialise database version")?;
+
+                if version > DB_VERSION {
+                    return Err(anyhow!(
+                        "database was created in a newer version of the registry (db version {version})"
+                    ));
+                }
+                if version < DB_VERSION {
+                    warn!("database was created in an older version of the registry (db version {version})");
+                    db.insert(DB_VERSION_KEY, bincode::serialize(&DB_VERSION)?)
+                        .with_context(|| "could not update database version in database")?;
+                    // TODO: Database migrations
+                }
+            }
+            None => {
+                // Database was empty
+                db.insert(DB_VERSION_KEY, bincode::serialize(&DB_VERSION)?)
+                    .with_context(|| "could not set database version in database")?;
+            }
+        }
+
+        let crate_tree = db.open_tree("crates")?;
+
+        Ok(Db { crate_tree })
     }
 
     pub fn get_crate(&self, crate_name: &str) -> Result<Option<Entry>, anyhow::Error> {
-        self.inner
+        self.crate_tree
             .get(crate_name)
             .with_context(|| "could not access cache entry")?
             .map(|raw| bincode::deserialize(&raw))
@@ -26,14 +56,14 @@ impl Db {
     }
 
     pub fn remove_crate(&self, crate_name: &str) -> Result<(), anyhow::Error> {
-        self.inner
+        self.crate_tree
             .remove(crate_name)
             .with_context(|| "could not remove entry from cache")
             .map(|_| ())
     }
 
     pub fn insert_crate(&self, crate_name: &str, entry: Entry) -> Result<(), anyhow::Error> {
-        self.inner
+        self.crate_tree
             .insert(
                 crate_name,
                 bincode::serialize(&entry).with_context(|| "could not serialise cache entry")?,
@@ -43,6 +73,6 @@ impl Db {
     }
 
     pub fn iter_crates(&self) -> sled::Iter {
-        self.inner.iter()
+        self.crate_tree.iter()
     }
 }
